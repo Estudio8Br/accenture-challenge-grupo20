20cbcedf74c4ca31fc14392c5be5cdbee1cda4e0
emerson <emerson.caique@outlook.com>
2020-10-03 18:06:45 -0300

    melhorias

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,7 +10,71 @@
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-        <link rel="stylesheet" href="{{ url('/css/teclado.css') }}">
+      <style>
+          .dnone {
+    display: none !important;
+}
+.keyboard {
+    width: 300px;
+}
+.keyboard .container {
+    display: table;
+    padding-bottom: 5vh;
+    margin: auto;
+}
+@media (max-width: 900px) {
+    .keyboard .container {
+        background: #fff;
+        z-index: -1;
+    }
+}
+.keyboard .container .row {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 15px;
+}
+.keyboard .container .row .col-sm-2 {
+    cursor: pointer;
+    position: relative;
+    display: inline-block;
+    width: 60px;
+    margin-left: 12px;
+    margin-right: 12px;
+    border-radius: 30px;
+    overflow: hidden;
+    padding: 5px;
+}
+.keyboard .container .row .col-sm-2::after,
+.keyboard .container .row .col-sm-2::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+    transition: all 0.25s ease;
+    border-radius: 30px;
+}
+.keyboard .container .row .col-sm-2:hover::before {
+    box-shadow: inset 0 0 0 1px #2d2c3e;
+}
+.keyboard .container .row .col-sm-2 .numero {
+    font-weight: 600;
+    font-size: 16px;
+}
+.keyboard .container .row .col-sm-2 .letras {
+    font-size: 10px;
+    display: flex;
+    bottom: 5px;
+    position: absolute;
+    left: 15px;
+}
+.keyboard .container .row .col-sm-2 .number {
+    cursor: pointer;
+}
+
+      </style>
 <body>
 <div class="container">
     <div class="col-3 m-auto">
