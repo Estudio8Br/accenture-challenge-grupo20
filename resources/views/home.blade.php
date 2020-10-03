3da50861787538d04d78643fb30355d4885666cc
emerson <emerson.caique@outlook.com>
2020-10-03 18:18:21 -0300

    Melhorias

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Accenture Challenge - Grupo 20</title>
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
@@ -231,6 +232,10 @@ window.addEventListener("DOMContentLoaded", function(event) {
                 word.value = word.value + data;
             }
         );
+        
+        axios.post("{{ route('get-letter') }}").then( data => {
+            word.value = word.value + data;
+        })
         
 
         //numbers.value = ''
